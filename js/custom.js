3173af1b83daf4c60fa95b0073e814dd2ef8d1c3
Whispy <dan.berkowitz1@gmail.com>
2014-07-25 08:53:33 -0400

    Hiding explore/action on load and revealing on cta click

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,28 @@
 $(document).ready(function () {
 
+	var explore = $('#explore');
+	var action = $('#action');
+	var callToActionExplore = $('.cta a[href="#explore"]');
+	var callToActionAction = $('.cta a[href="#action"]');
+
+	explore.addClass('resetDiv');
+	action.addClass('resetDiv');
+
+	callToActionExplore.on('click', function(){
+		explore.removeClass('resetDiv');
+		action.addClass('resetDiv');
+	})
+
+	callToActionAction.on('click', function(){
+		action.removeClass('resetDiv');
+		explore.addClass('resetDiv');
+	})
+
+
 	var $root = $('html, body');
 	$('a').click(function() {
 	    var href = $.attr(this, 'href');
 	    var offsetPlus = $(href).offset().top + 1 + 'px';
-	    console.log(offsetPlus);
 	    $root.animate({
 	        scrollTop: offsetPlus
 	    }, 500, function () {
